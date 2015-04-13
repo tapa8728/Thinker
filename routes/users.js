26df27cd63cb35fdce4017da19349169b4f88f43
spencehood <spho6674@colorado.edu>
2015-04-13 17:09:37 -0600

    quiz page is currently called profile.ejs and we have updated courses

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/login', function(req, res, next) {
-	res.render('Login', {title:'me'});
-});
-
-module.exports = router;
---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var configDB =require('./config/database.js');
+mongoose.connect(configDB.url);
+ require('./config/passport')(passport); // pass passport for configuration
+
+app.use(morgan('dev')); //log every request to the console 
+app.use(cookieParser()); //read cookies (needed for auth)
+app.use(bodyParser()); //get information from html forms 
+
+app.set('view engine', 'ejs');
+
+//required for passport 
+app.use(session({ secret: 'ilovescotchscotchyscotchscothch'}));
+app.use(passport.initialize());
+app.use(passport.session()); //persistent login sessions
+app.use(flash()); //use connect-flash for flash messages stored in session
+
+//routes 
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+// launch
+app.listen(port);
+console.log('The magic happens on port ' + port);
+
+
+
